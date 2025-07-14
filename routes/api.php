44896dc96b2ecc2f9137544c291916d3720b1c79
KnowRise <rifaasiraajuddin.123@gmail.com>
2025-07-14 08:09:22 +0700

    Asli DONE

--- routes/api.php
+++ routes/api.php
@@ -9,24 +9,26 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/oauth/google', [AuthController::class, 'oAuthUrl']);
-    Route::get('/oauth/google/callback', [AuthController::class, 'oAuthCallback']);
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/me', [AuthController::class, 'me']);
-        Route::post('/logout', [AuthController::class, 'logout']);
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::get('/oauth/google', [AuthController::class, 'oAuthUrl']);
+        Route::get('/oauth/google/callback', [AuthController::class, 'oAuthCallback']);
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('/me', [AuthController::class, 'me']);
+            Route::post('/logout', [AuthController::class, 'logout']);
+        });
     });
+    Route::get('plans', [PlanController::class, 'index']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::apiResource('tasks', TaskController::class)->only(['index', 'store', 'show', 'destroy']);
+        Route::post('tasks/{id}', [TaskController::class, 'update']);
+        Route::apiResource('subtasks', SubtaskController::class)->only(['index', 'store', 'destroy']);
+        Route::post('subtasks/{id}', [SubtaskController::class, 'update']);
+        Route::post('subtasks', [SubtaskController::class, 'changeStatus']);
+        Route::apiResource('orders', OrderController::class)->only(['index', 'store', 'show', 'destroy']);
+        Route::apiResource('payments', PaymentController::class)->only(['index', 'store', 'show']);
+    });
+    Route::post('/payments/callback', [PaymentController::class, 'callback']);
 });
-Route::get('plans', [PlanController::class, 'index']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('tasks', TaskController::class)->only(['index', 'store', 'show', 'destroy']);
-    Route::post('tasks/{id}', [TaskController::class, 'update']);
-    Route::apiResource('subtasks', SubtaskController::class)->only(['index', 'store', 'destroy']);
-    Route::post('subtasks/{id}', [SubtaskController::class, 'update']);
-    Route::post('subtasks', [SubtaskController::class, 'changeStatus']);
-    Route::apiResource('orders', OrderController::class)->only(['index', 'store', 'show', 'destroy']);
-    Route::apiResource('payments', PaymentController::class)->only(['index', 'store', 'show']);
-});
-Route::post('/payments/callback', [PaymentController::class, 'callback']);
