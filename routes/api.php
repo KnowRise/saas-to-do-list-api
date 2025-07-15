36dc8844a29409ed40006955eef1f793e17f95b0
uiahan <farhandikaalsani@gmail.com>
2025-07-15 10:31:09 +0700

    menambahkan beberapa endpoint baru

--- routes/api.php
+++ routes/api.php
@@ -6,43 +6,51 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\PlanController;
 use App\Http\Controllers\SubtaskController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    // Auth
-    Route::prefix('auth')->group(function () {
-        Route::post('/register', [AuthController::class, 'register']);
-        Route::post('/login', [AuthController::class, 'login']);
-        Route::get('/oauth/google', [AuthController::class, 'oAuthUrl']);
-        Route::get('/oauth/google/callback', [AuthController::class, 'oAuthCallback']);
-        
-        Route::middleware('auth:sanctum')->group(function () {
-            Route::get('/me', [AuthController::class, 'me']);
-            Route::post('/logout', [AuthController::class, 'logout']);
-        });
-    });
+  // Auth
+  Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/oauth/google', [AuthController::class, 'oAuthUrl']);
+    Route::get('/oauth/google/callback', [AuthController::class, 'oAuthCallback']);
 
-    // Public plans
-    Route::get('plans', [PlanController::class, 'index']);
-
-    // Protected Routes
     Route::middleware('auth:sanctum')->group(function () {
-        // Tasks
-        Route::apiResource('tasks', TaskController::class)->only(['index', 'store', 'show', 'destroy']);
-        Route::post('tasks/{id}', [TaskController::class, 'update']);
-
-        // Subtasks
-        Route::post('/subtasks/change-status', [SubtaskController::class, 'changeStatus']);
-        Route::apiResource('subtasks', SubtaskController::class)->only(['index', 'destroy']);
-        Route::post('subtasks', [SubtaskController::class, 'store']); // CREATE
-        Route::post('subtasks/{id}', [SubtaskController::class, 'update']); // UPDATE
-
-        // Orders
-        Route::apiResource('orders', OrderController::class)->only(['index', 'store', 'show', 'destroy']);
-
-        // Payments
-        Route::apiResource('payments', PaymentController::class)->only(['index', 'store', 'show']);
+      Route::get('/me', [AuthController::class, 'me']);
+      Route::post('/logout', [AuthController::class, 'logout']);
     });
+  });
 
-    Route::post('/payments/callback', [PaymentController::class, 'callback']);
+  Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user/me', [UserController::class, 'me']);
+    Route::post('/user/update', [UserController::class, 'update']);
+    Route::delete('/user/delete-avatar', [UserController::class, 'deleteAvatar']);
+    Route::post('/user/change-password', [UserController::class, 'changePassword']);
+  });
+
+  // Public plans
+  Route::get('plans', [PlanController::class, 'index']);
+
+  // Protected Routes
+  Route::middleware('auth:sanctum')->group(function () {
+    // Tasks
+    Route::apiResource('tasks', TaskController::class)->only(['index', 'store', 'show', 'destroy']);
+    Route::post('tasks/{id}', [TaskController::class, 'update']);
+
+    // Subtasks
+    Route::post('/subtasks/change-status', [SubtaskController::class, 'changeStatus']);
+    Route::apiResource('subtasks', SubtaskController::class)->only(['index', 'destroy']);
+    Route::post('subtasks', [SubtaskController::class, 'store']); // CREATE
+    Route::post('subtasks/{id}', [SubtaskController::class, 'update']); // UPDATE
+
+    // Orders
+    Route::apiResource('orders', OrderController::class)->only(['index', 'store', 'show', 'destroy']);
+
+    // Payments
+    Route::apiResource('payments', PaymentController::class)->only(['index', 'store', 'show']);
+  });
+
+  Route::post('/payments/callback', [PaymentController::class, 'callback']);
 });
